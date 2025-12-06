1a21398dd9675e8d587b72a26f0b72f303b12de4
arafsk <arafsos@protonmail.com>
2025-12-06 22:32:36 +0600

    update

--- setup-git.sh
+++ setup-git.sh
@@ -2,18 +2,15 @@
 
 project=$(basename `pwd`)
 echo "-----------------------------------------------------------------------------"
-echo "this is project https://github.com/arafsk/"$project
+echo "this is project https://github.com/arafsk/'$project'"
 echo "-----------------------------------------------------------------------------"
 git config --global pull.rebase false
 git config --global user.name "arafsk"
 git config --global user.email "arafsos@protonmail.com"
 sudo git config --system core.editor nano
 git config --global push.default simple
-
-git remote set-url origin git@github.com:buildra/$project
-
+git remote set-url origin git@github.com:arafsk/$project
 echo "Everything set"
-
 echo
 tput setaf 6
 echo "##############################################################"
---
+++ x86_64/updaterepo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+rm araflinux_repo*
+
+echo "repo-add"
+repo-add -n -R araflinux_repo.db.tar.gz ./*.zst
+
+sleep 1
+
+rm araflinux_repo.db
+
+rm araflinux_repo.files
+
+mv araflinux_repo.db.tar.gz araflinux_repo.db
+
+mv araflinux_repo.files.tar.gz araflinux_repo.files
+
+echo "####################################"
+echo "Repo Updated!!"
+echo "####################################"
