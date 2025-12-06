fa5a78e51f3e2f0c63c1dd0bd65be0fd3668f57b
arafsk <arafsos@protonmail.com>
2025-12-07 01:22:38 +0600

    updates

---
+++ set-git.sh
@@ -0,0 +1,7 @@
+git config --global pull.rebase false
+git config --global user.name "arafsk"
+git config --global user.email "arafsos@protonmail.com"
+sudo git config --system core.editor nano
+
+git config --global push.default simple
+git remote set-url origin git@github.com:arafsk/araflinux_repo
---
+++ update-all.sh
@@ -0,0 +1,10 @@
+# Add All Changes 
+git config --global push.default simple
+
+git add .
+
+# Committing to the local repository with a message containing the time details and commit text
+git commit -m "update"
+
+# Push the local files to github
+git push
