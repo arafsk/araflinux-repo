6fe2f478ef13c6264d66c250960e8cbd516dc159
arafsk <arafsos@protonmail.com>
2025-12-07 01:42:44 +0600

    update

--- set-git.sh
+++ set-git.sh
@@ -2,6 +2,6 @@ git config --global pull.rebase false
 git config --global user.name "arafsk"
 git config --global user.email "arafsos@protonmail.com"
 sudo git config --system core.editor nano
-
 git config --global push.default simple
-git remote set-url origin git@github.com:arafsk/araflinux_repo
+
+# git remote set-url origin 
