e397840c665c0a7013bb1a7d54c2ccbba25cd8fb
arafsk <arafsos@protonmail.com>
2025-12-06 22:06:56 +0600

    Update and rename setup-git-v5.sh to setup-git.sh

--- setup-git.sh
+++ setup-git.sh
@@ -6,11 +6,11 @@ echo "this is project https://github.com/arafsk/"$project
 echo "-----------------------------------------------------------------------------"
 git config --global pull.rebase false
 git config --global user.name "arafsk"
-git config --global user.email "arafos@protonmail.com"
+git config --global user.email "arafsos@protonmail.com"
 sudo git config --system core.editor nano
 git config --global push.default simple
 
-git remote set-url origin git@github.com:arafsk/$project
+git remote set-url origin git@github.com:buildra/$project
 
 echo "Everything set"
 
